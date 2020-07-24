8bf8c6d2f2a78cdd12faa9b862c43432291e8361
xp <xp@qq.com>
2020-07-25 07:43:06 +0800

    For the first time to import

---
+++ src/com/codeTest/domain/Person.java
@@ -0,0 +1,14 @@
+package com.codeTest.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.codeTest.domain
+ * Description:
+ *
+ * @date:2020/7/25 7:31
+ * @author:xupeng
+ */
+public class Person {
+
+    private String id;
+}
